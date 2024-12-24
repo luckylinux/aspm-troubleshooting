5075ecfb1b2b5ac1430a094273b6e3c0fdcf4d02
luckylinux <luckylinux>
2024-12-24 01:57:18 +0100

    Add another version of turbostat Command.

--- get_system_info.sh
+++ get_system_info.sh
@@ -23,3 +23,6 @@ grep . /sys/devices/system/cpu/cpuidle/*
 
 # Display Statistics using turbostat every 0.5 Seconds
 turbostat --show Avg_MHz,Busy%,Bzy_MHz,TSC_MHz,POLL,POLL%,C1%,C1E%,C3%,C6%,C7s%,CPU%c1,CPU%c3,CPU%c6,CPU%c7,Pkg%pc2,Pkg%pc3,Pkg%pc6,Pkg%pc7 --interval 0.5
+
+# Display Statistics using turbostat every 0.5 Seconds
+turbostat --show Avg_MHz,Busy%,Bzy_MHz,TSC_MHz,POLL,POLL%,C1%,C1E%,C3%,C6%,C7s%,CPU%c1,CPU%c3,CPU%c6,CPU%c7,Pkg%pc2,Pkg%pc3,Pkg%pc6,Pkg%pc7,PkgWatt,CorWatt,IPC,IRQ,SMI --interval 0.5
